e9650b104edeaf0512ca109f5ad554b4db5d96f8
suchov <artem.suchov@gmail.com>
2018-04-01 22:53:34 +0300

    Handle article not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found; end
 end
